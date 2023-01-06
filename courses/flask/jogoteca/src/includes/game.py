b16e368d28f14748b5a816dc6c59eae105023fd4
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-01-06 17:53:05 -0300

    feat: criação da classe Game

---
+++ courses/flask/jogoteca/src/includes/game.py
@@ -0,0 +1,10 @@
+class Game:
+    def __init__(
+        self,
+        name: str,
+        category: str,
+        platform: str
+    ) -> None:
+        self.name = name
+        self.category = category
+        self.platform = platform
